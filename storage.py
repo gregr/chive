4d87b49b22824542add54085bbb83a2a58dd25ec
Gregory L. Rosenblatt <greg.weiqi@gmail.com>
2010-02-16 19:56:51 -0800

    more work on storage

--- storage.py
+++ storage.py
@@ -14,29 +14,43 @@
 
 class Layout:
     def __repr__(self): return '<%s %s>'%(self.__class__.__name__, self.desc())
+    def index(self, idx): raise NotImplementedError
 class ScalarLayout(Layout):
-    def __init__(self, primValTag): self.tag = primValTag
-    def desc(self): return str(self.tag)
+    def __init__(self, name): self.name = name
+    def desc(self): return str(self.name)
     def size(self): return 1 # fixed size due to python
-class TupleLayout(Layout):
+class StructLayout(Layout):
     def __init__(self, ellyts): self.ellyts = ellyts
     def desc(self): return '#{%s}'%' '.join(lyt.desc() for lyt in self.ellyts)
     def size(self): return sum(lyt.size() for lyt in self.ellyts)
-    def offset(self, n):
-        assert n<len(self.elts)
-        return sum(lyt.size() for lyt in self.ellyts[:n])
+    def index(self, idx):
+        assert idx>=0 and idx<len(self.ellyts), (idx, self.desc())
+        return self.ellyts[idx], sum(lyt.size() for lyt in self.ellyts[:idx])
 class ArrayLayout(Layout):
-    def __init__(self, ellyt, num): self.ellyt = ellyt; self.num = num
+    def __init__(self, ellyt, cnt): self.ellyt = ellyt; self.cnt = cnt
     def desc(self):
-        if self.num is None: pref = ''
-        else: pref = '%d * '%self.num
+        if self.cnt is None: pref = ''
+        else: pref = '%d * '%self.cnt
         return '#[%s]'%(pref+self.ellyt.desc())
-    def size(self): return self.num*self.ellyt.size()
-    def offset(self, n): # None-length array implies dynamic extent
-        assert self.num is None or n<self.num, n; return n*self.ellyt.size()
-def mem_alloc(lyt): return (0, [None]*lyt.size())
-def mem_offset(mem, off):
-    off = mem[0]+off; assert off < len(mem[1]), (off, len(mem[1])
-    return (off, mem[1])
-def mem_deref(mem): return mem[1][mem[0]]
+    def size(self):
+        assert self.cnt is not None; return self.cnt*self.ellyt.size()
+    def index(self, idx): # None-length array implies dynamic extent
+        assert self.cnt is None or idx>=0 and idx<self.cnt, (idx, self.desc())
+        return self.ellyt, idx*self.ellyt.size()
+def mem_alloc(sz): return (0, [None]*sz)
+def mem_offset_(mem, off): return mem[0]+off
+def mem_offset(mem, off): return (mem_offset_(mem, off), mem[1])
+def mem_read(mem, idx): return mem[1][mem_offset_(mem, idx)]
+def mem_write(mem, idx, val): mem[1][mem_offset_(mem, idx)] = val
+def mem_copy(src, dst, sz):
+    doff, ddat = dst; soff, sdat = src; ddat[doff:doff+sz]=sdat[soff:soff+sz]
+def data_offset(mem, lyt, idx):
+    lyt, off = lyt.index(idx); return lyt, mem_offset(mem, off)
+
+if __name__=='__main__':
+    ilyt = ScalarLayout('int')
+    alyt = ArrayLayout(ilyt, 5)
+    slyt = StructLayout((ilyt, ilyt, ilyt, alyt))
+    aalyt = ArrayLayout(slyt, 4)
+    dalyt = ArrayLayout(ilyt, None)
 
