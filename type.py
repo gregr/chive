231b8efb2bb7a56cb7c0149d8e7dfca4ed5541a3
Gregory L. Rosenblatt <greg.weiqi@gmail.com>
2010-02-21 11:30:22 -0800

    subtyping in progress

--- type.py
+++ type.py
@@ -22,7 +22,7 @@ def typed(ty, val): return (ty, val)
 def getTy(val): return val[0]
 def getVal(val): return val[1]
 class Type:
-    def contains(self, ty): return self is ty
+    def contains(self, ty, tenv=None): return self is ty
     def checkTy(self, val):
         if not self.contains(getTy(val)):
             typeErr(None, "expected '%s', given '%s'"%
@@ -45,6 +45,7 @@ class Type:
         self.checkTy(agg); elt, off = self.index(idx);
         elt.pack(getVal(agg), off, val)
     def desc(self): raise NotImplementedError
+    def __str__(self): return self.desc()
     def __repr__(self): return '<%s %s>'%(self.__class__.__name__, self.desc())
 ################################################################
 # unboxed types
@@ -77,8 +78,8 @@ def cachedType(cls):
     return makeType
 class PtrType(AtomicUnboxedType):
     def __init__(self, elt): self.elt = elt
-    def contains(self, ty):
-        return type(ty) is type(self) and self.elt.contains(ty.elt)
+    def contains(self, ty, tenv=None):
+        return type(ty) is type(self) and self.elt.contains(ty.elt, tenv)
     def desc(self): return '&%s'%self.elt
 ptrType = cachedType(PtrType)
 class AggUnboxedType(UnboxedType):
@@ -87,8 +88,8 @@ class AggUnboxedType(UnboxedType):
         mem_copy(mem_offset(mem, offset), val, self.size())
 class ArrayType(AggUnboxedType):
     def __init__(self, elt, cnt=None): self.elt = elt; self.cnt = cnt
-    def contains(self, ty):
-        return (type(ty) is type(self) and self.elt.contains(ty.elt)
+    def contains(self, ty, tenv=None):
+        return (type(ty) is type(self) and self.elt.contains(ty.elt, tenv)
                 and (ty.cnt == self.cnt or
                     (ty.cnt is not None and ty.cnt > self.cnt)))
     def size(self):
@@ -108,9 +109,10 @@ def struct_index(struct, idx):
     return struct.elts[idx], sum(elt.size() for elt in struct.elts[:idx])
 class StructType(AggUnboxedType):
     def __init__(self, elts): self.elts = elts
-    def contains(self, ty):
+    def contains(self, ty, tenv=None):
         return (type(ty) is type(self) and
-                all(t1.contains(t2) for t1, t2 in zip(self.elts, ty.elts)))
+                all(t1.contains(t2, tenv)
+                    for t1, t2 in zip(self.elts, ty.elts)))
     def size(self): return sum(elt.size() for elt in self.elts())
     def numIndices(self): return len(self.elts)
     def index(self, idx): return struct_index(self, idx)
@@ -125,13 +127,19 @@ class BoxedType(Type):
     def pack(self, mem, offset, box):
         self.checkTy(box); mem_write(mem, offset, box)
 class AnyType(BoxedType):
-    def contains(self, ty): return isinstance(ty, BoxedType)
+    def contains(self, ty, tenv=None): return isinstance(ty, BoxedType)
     def desc(self): return 'Any'
 anyTy = AnyType()
 class NodeType(BoxedType):
     def __init__(self, name, elts):
         self.name = name; self.elts = elts;
         if elts is not None: self.eltSize = sum(elt.size() for elt in elts)
+    def contains(self, ty, tenv=None):
+        if isinstance(ty, RefinedType):
+            return (ty.elt is self and
+                    all(elt.contains(arg, tenv)
+                        for elt, arg in zip(self.elts, ty.args)))
+        return ty is self
     def checkValid(self):
         if self.elts is None:
             typeErr(None, "attempted to use undefined tag: '%s'"%self.name)
@@ -145,9 +153,19 @@ class NodeType(BoxedType):
             elt.pack(mem, offset, arg); offset += elt.size()
         return typed(self, mem)
     def desc(self): return str(self.name)
+    def refine(self, args):
+        assert len(args) == len(self.elts)
+        for arg, elt in zip(args, self.elts):
+            if arg is None: arg = elt
+            elif not elt.contains(arg):
+                typeErr(None, 'cannot refine node type')
+        return RefinedType(self, args)
 def isNode(v): return isTyped(v) and anyTy.contains(getTy(v))
 class ProcType(BoxedType):
     def __init__(self, inTy, outTy): self.inTy = inTy; self.outTy = outTy
+    def contains(self, ty, tenv=None):
+        return (isProc(ty) and self.inTy.contains(ty.inTy, tenv) and
+                self.outTy.contains(ty.outTy, tenv))
     def new(self, proc): return typed(self, proc)
     def appliedTy(self, remainingApps):
         ty = self; argts = []
@@ -156,29 +174,64 @@ class ProcType(BoxedType):
             argts.append(ty.inTy); ty = ty.outTy; remainingApps -= 1
         return ty, argts, remainingApps
     def desc(self): return '%s -> %s'%(self.inTy.desc(), self.outTy.desc())
+    def refine(self, rIn, rOut):
+        rpt = procType(rIn, rOut)
+        if self.contains(rpt): return rpt
+        typeErr(None, 'cannot refine proc type')
 def isProc(v): return isTyped(v) and isinstance(getTy(v), ProcType)
 procType = cachedType(ProcType)
 def curryProcType(paramts, rett):
     for paramt in paramts: rett = procType(paramt, rett)
     return rett
-class UnionType(BoxedType):
+class UnionType(BoxedType): # todo: refinement produces union of refinements
     def __init__(self, elts): self.elts = elts
-    def contains(self, ty):
+    def contains(self, ty, tenv=None):
         return ((isinstance(ty, UnionType) and
-                 all(self.contains(elt) for elt in ty.elts))
-                or any(elt.contains(ty) for elt in self.elts))
+                 all(self.contains(elt, tenv) for elt in ty.elts))
+                 or any(elt.contains(ty, tenv) for elt in self.elts))
     def desc(self): return '(%s)'%'|'.join(elt.desc() for elt in self.elts)
-class VarType(BoxedType):
+def intersectCns(cns, ty, tenv):
+    if cns[0].contains(ty, tenv): cns[0] = ty; return True # todo: reduce ty
+    return False
+class ParamVarType(BoxedType): # only makes sense within parametric context
+    def __init__(self, name): self.name = name
+    def contains(self, ty, tenv=None):
+        assert tenv is not None; constraint = tenv.get(self.name)
+        return intersectCns(constraint, ty, tenv)
+   def desc(self): return str(self.name)
+class ConstraintType(BoxedType):
     def __init__(self, name, constraint=anyTy):
         self.name = name; self.constraint = constraint
-    def contains(self, ty): # todo: separate containment for types vs. tags
-        return ((isinstance(ty, PolyType) and
-                 self.constraint.contains(ty.constraint))
-                or self.constraint.contains(ty))
-    def desc(self): return str(self.name)
+    def contains(self, ty, tenv=None):
+        return self.constraint.contains(ty, tenv)
+    def desc(self): return '%s<:%s'%(str(self.name), self.constraint)
 class ParametricType(BoxedType):
-    def __init__(self, name, params, elt):
-        self.name = name; self.params = params; self.elt = elt
-    def contains(self, ty): pass
+    def __init__(self, name, constraints, elt):
+        self.name = name; self.constraints = constraints; self.elt = elt
+    def contains(self, ty, tenv=None): # todo
+        if isinstance(ty, ParametricType): return ty is self
+        elif isinstance(ty, RefinedType):
+            if isinstance(ty, ParametricType):
+                return (ty.elt is self and
+                        all(cns.contains(arg, tenv)
+                            for cns, arg in zip(self.constraints, ty.args)))
+        assert tenv is not None; tenv = Env(tenv)
+        for cns in self.constraints: tenv.add(cns.name, [cns.constraint])
+        return self.elt.contains(ty, tenv)
     def desc(self):
-        return '(%s)'%' '.join([str(self.name), ' '.join(self.params)])
+        return '(%s => %s)'%(' '.join([str(self.name), ' '.join(self.params)]), self.elt.desc())
+class RefinedType(BoxedType):
+    def __init__(self, elt, args): self.elt = elt; self.args = args
+    def contains(self, ty, tenv):
+        if isinstance(ty, RefinedType):
+            return (self.elt is ty.elt and
+                    all(arg.contains(targ, tenv)
+                        for arg, targ in zip(self.args, ty.args)))
+        elif isinstance(self.elt, ParametricType):
+            assert tenv is not None; tenv = Env(tenv)
+            for cns, arg in zip(self.elt.constraints, self.args):
+                tenv.add(cns.name, [arg])
+            return self.elt.elt.contains(ty, tenv)
+        return False
+    def desc(self):
+        return '(%s)'%' '.join([self.elt.desc(), ' '.join(self.args)])
