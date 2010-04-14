158f18cf80a0769b7caabbe0e704c7551c1e0d48
Gregory L. Rosenblatt <greg.weiqi@gmail.com>
2010-04-13 18:41:21 -0700

    fixed bug in null-op syntax

--- syntax.py
+++ syntax.py
@@ -166,7 +166,7 @@ class Operator:
         assert isSymbol(sym), sym
         assert type(prec) is int, prec
         assert isSymbol(assoc), assoc
-        assert assoc in [symbol(nm) for nm in ('left', 'right', 'non')]
+        assert assoc in [symbol(nm) for nm in ('left', 'right', 'none')]
         self.sym = sym
         self.assocRight = assoc is symbol('right') # todo: non-associative ops
         self.prec = prec
